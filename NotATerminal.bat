66a7b9ab5e556b3967624b49b0eaacfb36f6e877
Minion3665 <pixtopia@null.net>
2019-04-29 08:40:28 +0100

    ..

--- NotATerminal.bat
+++ NotATerminal.bat
@@ -2,11 +2,22 @@
 title NotATermial.Anti.Filter
 color a
 cls
+
+set Callbacks["cls"]=:advanced_clear
+set Callbacks["clear"]=:advanced_clear
+
 :loop
 set /p command="Minion3665@NotATermial.Anti.Filter >>> "
 IF EXIST .\customCommands\%command%.bat (
-  call .\customCommands\%command%.bat
+  echo %command%
+  echo %Callbacks%
+  echo %Callbacks[command]%
+  call %Callbacks[command]%
 ) ELSE (
   call %command%
 )
 goto loop
+
+:advanced_clear
+  echo awjkdfjla
+EXIT /B 0
